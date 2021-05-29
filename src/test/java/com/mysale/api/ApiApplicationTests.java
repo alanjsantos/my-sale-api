d6fddbfc675d9f49c963e795d48b7dd29dd434d4
alan santos <alanjosedossantos011@gmail.com>
2021-05-29 09:20:32 -0300

    first commit

---
+++ src/test/java/com/mysale/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysale.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
